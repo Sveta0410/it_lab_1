cc22ebf0d8554060f6a8c0fdc161dc2ec4e53f2e
Sveta <sveta.zheleznova.03@mail.ru>
2022-09-30 23:25:46 +0300

    my first commit

---
+++ src/Palindrome.java
@@ -0,0 +1,27 @@
+// класс Palindrome предназначен для определениия, является ли слово палиндромом
+public class Palindrome {
+    // метод main предназначен для считывания входных данных и вывода на экран результата программы
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if (isPalindrome(s) == true){
+                System.out.println(s + " - палиндром");
+            }
+            else{
+                System.out.println(s + " - НЕ палиндром");
+            }
+        }
+    }
+    // метод reverseString предназначен для перестановки символов в строке в обратоном порядке
+    public static String reverseString(String s) {
+        String sReversed = "";
+        for (int i = (s.length()- 1); i > -1; i--){
+            sReversed += s.charAt(i);
+        }
+        return sReversed;
+    }
+    //метод isPalindrome предназначен для сравнения "перевёрнутой" строки с исходной
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+}
---
+++ src/Primes.java
@@ -0,0 +1,19 @@
+// класс Primes предназначен для поиска и вывода на экран всех простых чисел от 2 до 100
+public class Primes {
+    // метод main предназначен для вывода на экран всех простых чисел от 2 до 100
+    public static void main(String[] args) {
+        for (int j = 2; j < 101; j++) {
+            if (isPrime(j) == true) {
+                System.out.println(j);
+            }
+        }
+    }
+    // метод isPrime предназначен для проверки, является число простым или нет
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++)
+            if (n % i == 0) {
+                return false;
+            }
+        return true;
+    }
+}
